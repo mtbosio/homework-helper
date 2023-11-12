c232b9c8fa0a5a5c09cd58d843e1292e8ae1a7e0
Matthew <mattbosio08@gmail.com>
2023-11-11 17:18:43 -0800

    2

--- packages/express-backend/main.js
+++ packages/express-backend/main.js
@@ -38,9 +38,18 @@ app.get("/questions", cors(), (req, res) => {
   const subject = req.query.subject;
   const title = req.query.title;
   const author = req.query.author;
-  getQuestions(subject, title, author).then((response) =>
-    res.status(200).send(response),
-  );
+  getQuestions(subject, title, author).then((response) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, PATCH, DELETE",
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization",
+    );
+    res.status(200).send(response);
+  });
 });
 
 // Post new question

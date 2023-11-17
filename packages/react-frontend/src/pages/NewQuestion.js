3ff71df1cc7f179272ba99cbab1f963acb51d15a
EDT457 <tan.ethan.d@gmail.com>
2023-11-17 09:11:01 -0800

    fix

--- packages/react-frontend/src/pages/NewQuestion.js
+++ packages/react-frontend/src/pages/NewQuestion.js
@@ -1,16 +1,12 @@
 import React, { useState } from "react";
 import { postQuestion } from "../apis";
-<<<<<<< HEAD
 import { useNavigate } from "react-router-dom";
-
-function NewQuestion() {
-  const navigate = useNavigate();
-=======
 import Navbar from "../navbar";
 import "./NewQuestion.css";
 
+
 function NewQuestion() {
->>>>>>> 831a4a08 (frontend stuff)
+  const navigate = useNavigate();
   const [question, setQuestion] = useState({
     subject: "",
     title: "",
@@ -31,63 +27,16 @@ function NewQuestion() {
       subject: question.subject,
       title: question.title,
       author: question.author,
-<<<<<<< HEAD
       body: question.body,
     })
       .then((res) => {
         console.log(res);
         navigate("/");
       })
-=======
-      body: question.body
-    })
-      .then((res) => console.log(res))
->>>>>>> 831a4a08 (frontend stuff)
       .catch((exception) => console.log(exception));
   }
 
   return (
-<<<<<<< HEAD
-    <form onSubmit={handleSubmit}>
-      <label>
-        Title{" "}
-        <input
-          id="title"
-          type="text"
-          value={question.title}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Subject{" "}
-        <input
-          id="subject"
-          type="text"
-          value={question.subject}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Name{" "}
-        <input
-          id="author"
-          type="text"
-          value={question.author}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Question{" "}
-        <input
-          id="body"
-          type="text"
-          value={question.body}
-          onChange={handleChange}
-        />
-      </label>
-      <button type="submit">Post</button>
-    </form>
-=======
     <>
       <Navbar navbar>navbar</Navbar>
       <div classname="postform">
@@ -136,7 +85,6 @@ function NewQuestion() {
         </form>
       </div>
     </>
->>>>>>> 831a4a08 (frontend stuff)
   );
 }
 

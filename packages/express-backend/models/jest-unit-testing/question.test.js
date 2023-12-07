4dcc99a23192c77dd077a4dd61d4f1b3a722ca85
Matthew <mattbosio08@gmail.com>
2023-12-06 18:12:36 -0800

    Started testing

---
+++ packages/express-backend/models/jest-unit-testing/question.test.js
@@ -0,0 +1,70 @@
+//const { connectDB, dropDB, dropCollections } = require("./setuptestdb");
+//const { Question } = require("../express-backend/models/question.js");
+import { connectDB, dropDB, dropCollections } from "./setuptestdb";
+import Question from "../question";
+import mongoose from "mongoose";
+beforeAll(async () => {
+  await connectDB();
+});
+
+afterAll(async () => {
+  await dropDB();
+});
+
+afterEach(async () => {
+  await dropCollections();
+});
+describe("Question Model", () => {
+  it("should create a question successfully", async () => {
+    let validQuestion = {
+      subject: "Math",
+      title: "Calc Hw",
+      date: new Date(),
+      author: "Matt",
+      body: "please help",
+      votes: 10,
+      comments: 10,
+    };
+    const newQuestion = new Question(validQuestion);
+    await newQuestion.save();
+    expect(newQuestion._id).toBeDefined();
+    expect(newQuestion.subject).toBe(validQuestion.subject);
+    expect(newQuestion.title).toBe(validQuestion.title);
+    expect(newQuestion.date).toBe(validQuestion.date);
+    expect(newQuestion.author).toBe(validQuestion.author);
+    expect(newQuestion.body).toBe(validQuestion.body);
+    expect(newQuestion.votes).toBe(validQuestion.votes);
+    expect(newQuestion.comments).toBe(validQuestion.comments);
+  });
+
+  it("should fail for question without required fields", async () => {
+    let invalidQuestion = {
+      subject: "Math",
+    };
+    try {
+      const newQuestion = new Question(invalidQuestion);
+      await newQuestion.save();
+    } catch (error) {
+      expect(error).toBeInstanceOf(mongoose.Error.ValidationError);
+      expect(error.errors.completed).toBe();
+    }
+  });
+  it("should fail for question with fields of wrong type", async () => {
+    let invalidQuestion = {
+      subject: 10,
+      title: "Calc Hw",
+      date: new Date(),
+      author: "Matt",
+      body: "please help",
+      votes: 10,
+      comments: 10,
+    };
+    try {
+      const newQuestion = new Question(invalidQuestion);
+      await newQuestion.save();
+    } catch (error) {
+      expect(error).toBeInstanceOf(mongoose.Error.ValidationError);
+      expect(error.errors.completed).toBe();
+    }
+  });
+});
---
+++ packages/express-backend/models/jest-unit-testing/setuptestdb.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+
+let mongo = null;
+
+const connectDB = async () => {
+  mongo = await MongoMemoryServer.create();
+  const uri = mongo.getUri();
+
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+};
+
+const dropDB = async () => {
+  if (mongo) {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+    await mongo.stop();
+  }
+};
+
+const dropCollections = async () => {
+  if (mongo) {
+    const collections = await mongoose.connection.db.collections();
+    for (let collection of collections) {
+      await collection.drop();
+    }
+  }
+};
+export { connectDB, dropDB, dropCollections };

b27b1040aba7d85dfd0e6dcca162a261ac652c79
charlesmoreno32 <charlesmoreno007@gmail.com>
2023-12-06 20:02:54 -0800

    inital comment testing

---
+++ packages/express-backend/models/comment.test.js
@@ -0,0 +1,34 @@
+import Comment from "./comment";
+import { connectDB, dropDB, dropCollections } from "./setuptestdb";
+
+beforeAll(async () => {
+  await connectDB();
+});
+
+afterAll(async () => {
+  await dropDB();
+});
+
+afterEach(async () => {
+  await dropCollections();
+});
+describe("Comment Model", () => {
+  test("should create a comment successfully", async () => {
+    let validComment = {
+      questionID: "123",
+      author: "Chalres Moreno",
+      body: "this ez lol get better",
+      date: new Date(),
+    };
+    const newComment = new Comment(validComment);
+    await newComment.save();
+    expect(newComment._id).toBeDefined();
+    expect(newComment.questionID).toBe(validComment.questionID);
+    expect(newComment.author).toBe(validComment.author);
+    expect(newComment.body).toBe(validComment.body);
+    expect(newComment.author).toBe(validComment.author);
+    expect(newComment.date).toBe(validComment.date);
+  });
+
+  
+});
